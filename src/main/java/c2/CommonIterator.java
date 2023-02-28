905dd83a0addc544180bbb421a0f2bffae96e59f
Ryan <yang.zhao@oceanus-plus.com>
2023-02-28 10:09:42 +0800

    c2 - 迭代器

---
+++ src/main/java/c2/CommonIterator.java
@@ -0,0 +1,51 @@
+package c2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import lombok.var;
+
+/**
+ * @author ryan
+ * @version Id: CommonIterator, v 0.1 2023/2/27 5:31 PM ryan Exp $
+ */
+public class CommonIterator<T> implements Iterable<T> {
+
+    private final List<T> list;
+
+    public CommonIterator(List<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public T next() {
+                return list.get((int) (list.size() * Math.random()));
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        var list = Arrays.asList("List", "Tree", "Array");
+        var gen = new CommonIterator<String>(list);
+
+        // for循环的会进入到iterator()方法，先判断hasNext,返回true，再执行next
+        for (var s : gen) {
+            System.out.println(s);
+        }
+
+        var it = gen.iterator();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(it.next());
+        }
+    }
+}
